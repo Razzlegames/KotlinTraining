b827085a41c140c55ba578a04ab6f8b88cb3a93f
Luce, Kyle <kluce@paypal.com>
2019-03-30 12:51:01 -0700

    Add solutions fixed tests

--- src/main/java/kotlintraining/lesson3/extensionfunctions/TryExtensions.kt
+++ src/main/java/kotlintraining/lesson3/extensionfunctions/TryExtensions.kt
@@ -3,8 +3,10 @@ package kotlintraining.lesson3.extensionfunctions
 
 /**
  * This is an example of an extension function
- *  String? is the type it's "extending", it takes a string, if the string it's extending
- *    (called a "dispatch receiver") is null or empty, it returns @p default
+ *  String? is the type it's "extending", called a receiver.
+ *
+ *  It takes a string, if the string it's extending ("receiver")
+ *     is null or empty, it returns @p default
  */
 fun String?.defaultIfEmpty(default: String) = if (this.isNullOrEmpty()) default else this
 
@@ -17,15 +19,47 @@ fun String?.defaultIfEmpty2(default: String) : String {
 }
 
 /**
- *  TODO create an extension function that counts returns odd integers in a List
- *    If the list is null return 0
+ *  TODO Exercise 1
+ *
+ *  Create an extension function that counts returns odd integers in a List
+ *    If the list is null return 0.
+ *
+ *    Bonus, Use List<T>.filter and other extension functions to build your solution!
+ *
+ *    WARNING Don't add any duplicate elements to this, so next exercise will pass
  */
-val NUMBER_LIST = listOf(5, 3, 1, 9, 6, 2, 12324, 333, 5, 99)
+val NUMBER_LIST = listOf(530, 3, 1, 9, 6, 2, 12324, 333, 5, 99, 1002)
 
 /**
- *  TODO create an extension function that creates a map from NUMBER_LIST where the first value is the key,
+ * Excercise 1 Solution
+ */
+fun List<Int>?.countOdd() = this?.filter { it % 2 != 0 }?.count() ?: 0
+
+
+/**
+ *  TODO Exercise 2
+ *
+ *  Create an extension function called "toChunkedMap()"
+ *
+ *  that creates a map from NUMBER_LIST where the first value is the key,
  *   second value is the value, for example {5 maps to 3}, {1 maps to 9}, {6 maps to 2}
  *   You can assume that if there's odd elements that the last element maps to null
+ *
+ * Hints:
+ *   Receiver Type to extend: List<T>
+ *   Returns: Map<T, T?>
+ *
+ *   Extension function List<T>.chunked(int) can be used
+ *
  */
 
+/**
+ * Exercise 2 Solution
+ */
+fun <T> List<T>.toChunkedMap() : Map<T, T?> {
+
+ return this.chunked(2)
+     .map { Pair(it.first(), if(it.size > 1) it.last() else null) }
+     .toMap()
+}
 
--- src/test/java/kotlintraining/lesson3/extensionfunctions/TryExtensionsTest.kt
+++ src/test/java/kotlintraining/lesson3/extensionfunctions/TryExtensionsTest.kt
@@ -28,13 +28,49 @@ class TryExtensionsTest {
         assertEquals(setString, setString.defaultIfEmpty(default))
     }
 
+    /**
+     *  Excercise 1 test
+     *    Change to
+     */
     @Test
     fun countAllOddNumbers() {
+        val NUMBER_OF_ODDS = calculateOddsOldStyle(NUMBER_LIST)
         print(NUMBER_LIST.countOdd())
 
-        assertEquals(7, NUMBER_LIST.countOdd())
+        assertEquals(NUMBER_OF_ODDS, NUMBER_LIST.countOdd())
+    }
+
+    /**
+     *  Exercise 2 test
+     *    Note that you can change NUMBER_LIST (adding or removing numbers)
+     *      and this should still pass
+     */
+    @Test
+    fun listToChunkedMapTest() {
+
+        val map = NUMBER_LIST.toChunkedMap()
+
+        for (i in 0 until NUMBER_LIST.size step 2) {
+                assertEquals(map.get(NUMBER_LIST[i]),
+                    if(i < NUMBER_LIST.size-1) NUMBER_LIST[i+1] else null)
+        }
+    }
+
+    /**
+     * This is old style just and not an extension function, just to demonstrate what is
+     *   required for Exercise 1
+     */
+    private fun calculateOddsOldStyle(list: List<Int>): Int {
+
+        var count = 0
+        for(element in list) {
+            if (element % 2 != 0) {
+                count++
+            }
+        }
+
+        return count
     }
 }
 
-fun List<Int>?.countOdd() = this?.filter { it % 2 != 0 }?.count() ?: 0
 
